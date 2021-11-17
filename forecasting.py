07b2902b7e34d593d4e2b4553d6742bfd3075247
peteryang2581 <47307724+peteryang2581@users.noreply.github.com>
2021-11-17 10:52:24 -0600

    Add files via upload

---
+++ forecasting.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 17 09:44:59 2021
+Forecasting:
+Using seasonal data to create a forecast for a player's performance versus opponent
+@author: pjyang4
+"""
+
+"""
+Get Seasonal Data
+"""
+
+"""
+Get Opponent Data (fppg modifier)
+"""
+
+"""
+Use Machine Learning Models to forecast fppg
+"""
+
+"""
+push data to sql database
+"""
+
---
+++ optimizer.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 17 09:49:56 2021
+
+@author: thyang
+"""
+
+"""
+Get Data from SQL
+"""
+
+"""
+Delete List
+"""
+
+"""
+Protect_list
+"""
+
+"""
+Optimizer
+"""
